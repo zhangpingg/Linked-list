d70e4e9ddab61e87da4b226d7df38ba1ea854188
张平 <feitian.zhang@tuya.com>
2021-12-24 16:13:37 +0800

    feat:双向链表

--- 02_双向链表.js
+++ 02_双向链表.js
@@ -1,22 +1,19 @@
-// 测试方式
-// 1) node 01_单向链表
-// 2) 放到html中的script中
-
-
 // Node 节点类
 function Node(element) {
   this.element = element;   // 当前节点的元素
   this.next = null;         // 下一个节点链接
+  this.previous = null;     // 上一个节点链接
 }
 
 // Linked-list 链表类
 function LList() {
   this.head = new Node('head');       // 用head属性，保存链表的头节点
   this.find = find;                   // 查找节点
+  this.findLast = findLast;           // 查找最后一个元素
   this.insert = insert;               // 插入节点
-  this.findPrev = findPrev;           // 查找前一个节点
   this.remove = remove;               // 删除节点
   this.display = display;             // 显示链表
+  this.dispReverse = dispReverse;     // 反向显示链表
 }
 // 查找给定节点
 function find(item) {
@@ -26,26 +23,30 @@ function find(item) {
   }
   return currNode;                      // 若找到，则返回包含该数据的节点，否则，返回null
 }
+//查找最后一个元素
+function findLast () {
+  var currNode = this.head;
+  while ( !( currNode.next == null )){
+      currNode = currNode.next;
+  }
+  return currNode;
+}
 // 插入节点
 function insert(newElement, item) {
   var newNode = new Node(newElement);   // 将新元素转换为Node节点
   var currNode = this.find(item);       // 找到要插入节点的位置（前驱）
   newNode.next = currNode.next;         // 前驱的next赋值给新节点的next（与后面节点建立联系）
+  newNode.previous = currNode;          // 设置新节点的 previous
   currNode.next = newNode;              // 新节点赋值给前驱的next（与前面节点建立联系）
-}
-// 查找带删除节点的前一个节点 (判断下一个节点是否是要找的节点)
-function findPrev(item) {
-  var currNode = this.head;
-  while (!(currNode.next == null) && (currNode.next.element != item)) {
-    currNode = currNode.next;
-  }
-  return currNode;
 }
 // 删除节点
 function remove(item) {
-  var prevNode = this.findPrev(item);
-  if (!(prevNode.next == null)) {
-    prevNode.next = prevNode.next.next;
+  var currNode = this.find(item);
+  if (!(currNode.next == null)) {
+    currNode.previous.next = currNode.next;
+    currNode.next.previous = currNode.previous;
+    currNode.next = null;
+    currNode.previous = null;
   }
 }
 // 显示链表元素
@@ -58,7 +59,16 @@ function display() {
   }
   return _result
 }
-
+// 反向显示链表元素
+function dispReverse () {
+  let _result = []
+  var currNode = this.findLast();
+  while ( !( currNode.previous == null )){
+      _result.push(currNode.element)
+      currNode = currNode.previous;
+  }
+  return _result
+}
 
 
 // 示例
@@ -69,6 +79,7 @@ fruits.insert('Pear', 'Banana');
 fruits.insert('Grape', 'Pear');
 fruits.remove('Banana');
 console.log(fruits.display())                   // ['Apple', 'Pear', 'Grape']
+console.log(fruits.dispReverse())               // [ 'Grape', 'Pear', 'Apple' ]
 
 
 
